d0d3b0ac12b2214d335aecad22cc70a8ea921abc
C2dan88 <wildteen88@googlemail.com>
2013-01-20 13:24:21 +0000

    Basic App Structure

---
+++ express.js
@@ -0,0 +1,29 @@
+express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	html  = '<h1>Express</h1><p>Welcome to my node.js express project!</p>';
+	html += '<h2>My Pages</h2>';
+		pages = ['hello.txt'];
+
+		links = '<ul>';
+		for(var i = 0; i < pages.length; ++i) {
+			links += '<li><a href="'+pages[i]+'">'+pages[i]+'</a></li>';
+		}
+		links += '</ul>';
+
+	html += links;
+
+	res.send(html);
+});
+
+app.get('/hello.txt', function(req, res) {
+	var body = 'Hello World!!!';
+	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Length', body.length);
+	res.end(body);
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
